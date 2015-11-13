d3507aecb4a4ace1a41b412ef360de5519b70668
wangenyong <wangenyong@gmail.com>
2015-11-13 13:56:08 +0800

    Project init.

---
+++ DsDiary/UIColorAdditions.swift
@@ -0,0 +1,15 @@
+//
+//  UIColorAdditions.swift
+//  DsDiary
+//
+//  Created by wangenyong on 15/11/13.
+//  Copyright © 2015年 ___WANGDASHUI___. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class func tintColor() -> UIColor {
+        return UIColor(red: 54.0/255.0, green: 171.0/255.0, blue: 169.0/255.0, alpha: 1.0)
+    }
+}
